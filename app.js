99017ae886dd0fb9f105a91a58e8541336117f30
Saim11223344 <saimshykh777@gmail.com>
2022-06-23 06:32:11 +0600

    some changes

--- app.js
+++ app.js
@@ -4,11 +4,34 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-
 let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
-
+let adminRouter = require('./routes/admin');
+const mongoose = require("mongoose");
+const appSession = require("express-session");
+const dbURL=require('./db/db.config');
 let app = express();
+app.use(
+  appSession({
+      secret: "#8#!]&>4}'@{_:/}",
+      saveUninitialized: true,
+      cookie: { maxAge: 86400 },
+      resave: false,
+  })
+);
+app.use(function(req, res, next) {
+  res.locals.admin = req.session.admin;
+  next();
+});
+//db connection
+mongoose.connect(dbURL.dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Database Connection Generated Error: "));
+db.once("open", function() {
+  console.log("Database Connection Made Successfully.");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,7 +45,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+function validationAdminArea(req, res, next) {
+  if (req.session.admin) {
+      next();
+  } else {
+      res.redirect("/login");
+  }
+}
+
+app.all("/admin/*", validationAdminArea, function(req, res, next) {
+  next();
+});
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
